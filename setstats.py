7358ec9d1bebe3aea2553401724cd4eff2a94405
James Cook <cookjw@gmail.com>
2014-11-13 15:45:53 -0800

    webern data

---
+++ setstats.py
@@ -0,0 +1,82 @@
+
+# Collect all set types; store as dictionary keys, with number of 
+# occurences as values.
+
+# Procedure:
+# copy text in between brackets
+# - start with copy mode off
+# - scan each character in the file
+# -- determine status of copy mode
+# -- determine whether character is a bracket
+# -- if character is a bracket, switch copy mode
+# -- otherwise, do the action appropriate to copy mode status
+# --- if off, do nothing
+# --- if on, copy
+
+f = open("webern_op5_no4_simultaneities.txt", 'r')
+
+file = f.read()
+
+copystring = ""
+
+copy_mode = 0
+
+def switch_copy_mode():
+    global copy_mode
+    copy_mode = (copy_mode + 1) %2
+
+for character in file:
+  if character in ["[", "]"]:
+      if copy_mode:
+          copystring += character + " "
+      switch_copy_mode()
+  if copy_mode:
+      copystring += character
+
+types = [item for item in copystring.split(' ') if item]
+
+occurences = {type: types.count(type) for type in types}
+
+print occurences
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        

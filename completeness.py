f6c4d089b377cf62d23976fc0437f2bb9f7a77da
James Cook <cookjw@gmail.com>
2014-11-12 17:18:03 -0800

    completeness.py, and started enumerate_set_types.py

---
+++ completeness.py
@@ -0,0 +1,30 @@
+# Checks whether a pc set is "complete", i.e. whether every pc is 
+# reachable by conjunct motion (interval <= 2) from some pc in the 
+# set. The central example, of course, is the triad (set-class [037]),
+# for which reason I had been in the habit of mentally referring to these
+# collections as "triadically complete"; but there exist collections not
+# containing triads -- in fact, even collections of cardinality 3, such as
+# those of type [027] -- that have this property. 
+
+
+def reachable_by_step(pc):
+    return [(x + pc) % 12 for x in range(-2,3)]
+    
+def is_complete(collection):
+    all_reachable_by_step = []
+    for pc in collection:
+        all_reachable_by_step += reachable_by_step(pc)
+    for pc in range(12):
+        if not pc in all_reachable_by_step:
+            return False
+    return True
+    
+    
+    
+print is_complete([0,3,7])
+
+print is_complete([0,2,7])
+
+print is_complete([0,1,6])
+
+    

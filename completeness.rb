de433218a8f6e8edf5619eb68803e1b1358fe0b3
James Cook <cookjw@gmail.com>
2014-11-12 16:04:56 -0800

    important correction

--- completeness.rb
+++ completeness.rb
@@ -1,10 +1,10 @@
 # Checks whether a pc set is "complete", i.e. whether every pc is 
 # reachable by conjunct motion (interval <= 2) from some pc in the 
-# set. The central example, of course, is the triad (set-class [0,3,7]),
+# set. The central example, of course, is the triad (set-class [037]),
 # for which reason I had been in the habit of mentally referring to these
 # collections as "triadically complete"; but there exist collections not
 # containing triads -- in fact, even collections of cardinality 3, such as
-# those of type [0,2,7] -- that have this property. 
+# those of type [027] -- that have this property. 
 
 
 def reachable_by_step(pc)
@@ -16,7 +16,7 @@ def complete?(collection)
   for pc in collection
     all_reachable_by_step += reachable_by_step(pc)    
   end
-  for pc in (0...11)
+  for pc in (0...12)
     unless all_reachable_by_step.include?(pc)
       return false
     end

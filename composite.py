e998dbc1b7906c8d311bb9aa06e7bd558b2e589e
James Cook <cookjw@gmail.com>
2015-01-08 13:50:43 -0800

    onset time calculator

--- composite.py
+++ composite.py
@@ -6,3 +6,11 @@ def composite_order(list_list):
     for list in list_list:
         superlist += list
     return sorted(superlist)
+    
+
+def get_onset_times(durational_values_list):
+    onset_times = [0]
+    for duration in durational_values_list[:-1]:
+        onset_times.append(onset_times[-1] + duration)
+    return onset_times
+
--- test_composite.py
+++ test_composite.py
@@ -1,5 +1,5 @@
 import unittest
-from composite import composite_order
+from composite import composite_order, get_onset_times
 
 class CompositeOrderTest(unittest.TestCase):
     def test_composite_order(self):
@@ -9,6 +9,15 @@ class CompositeOrderTest(unittest.TestCase):
         self.assertEqual(
         composite_order(list_list), [1,2,3,3,6,7,8,8,9]
         ) 
+    
+class OnsetTimeTest(unittest.TestCase):
+    def test_onset_times(self):
+        self.assertEqual(
+        get_onset_times([1,1,1,1]), [0,1,2,3]
+        )
+        self.assertEqual(
+        get_onset_times([1,2,1,1]), [0,1,3,4]
+        )
         
 
 

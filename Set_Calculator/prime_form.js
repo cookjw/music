6c6235a1adba8b55ecc1de3038b785e01b64d11e
James Cook <cookjw@gmail.com>
2015-01-24 15:48:16 -0800

    scaffolding gradually being put into place...

---
+++ Set_Calculator/prime_form.js
@@ -0,0 +1,29 @@
+
+
+
+function removeDuplicates(list){
+    var new_list = [];
+    for (item in new_list){
+        if ($.inArray(item, new_list) === -1){
+            new_list.push(item);        
+        }
+    }
+    return new_list
+}
+
+function copy(content){
+    var j = document.getElementById("output");
+    j.value = content;    
+}
+
+function businessLogic(input){ // this will eventually be findPrimeForm(set)
+    input = input.split(",")
+    return [0]
+    
+}
+
+function getResult(){
+    this.output.value = businessLogic(
+        this.input.value
+        )
+}

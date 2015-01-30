d8ab7f620a1ec3d7e3ee5c0ab5c8ce03fae447e6
James Cook <cookjw@gmail.com>
2015-01-29 21:58:51 -0800

    removed commented-out original equals function

--- Set_Calculator/primeForm.js
+++ Set_Calculator/primeForm.js
@@ -1,30 +1,7 @@
-// attach the .equals method to Array's prototype to call it on any array
-// (credit: http://stackoverflow.com/questions/7837456/comparing-two-arrays-in-javascript )
-// Array.prototype.equals = function (array) {
-    // // if the other array is a falsy value, return
-    // if (!array)
-        // return false;
 
-    // // compare lengths - can save a lot of time 
-    // if (this.length != array.length)
-        // return false;
-
-    // for (var i = 0, l=this.length; i < l; i++) {
-        // // Check if we have nested arrays
-        // if (this[i] instanceof Array && array[i] instanceof Array) {
-            // // recurse into the nested arrays
-            // if (!this[i].equals(array[i]))
-                // return false;       
-        // }           
-        // else if (this[i] != array[i]) { 
-            // // Warning - two different object instances will never be equal: {x:20} != {x:20}
-            // return false;   
-        // }           
-    // }       
-    // return true;
-// }  
-
-// "Prototyping" (monkeypatching the built-in class) didn't work, because it made it think every array
+// credit for the equals function: http://stackoverflow.com/questions/7837456/comparing-two-arrays-in-javascript ...
+// except that... 
+// "prototyping" (monkeypatching the built-in class) didn't work, because it made it think every array
 // had an "equals" attribute to be inspected every time the array was looped through; otherwise the
 // logic of the function below is the same.
 
